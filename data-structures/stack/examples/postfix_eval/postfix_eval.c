fa7148e1a2a95063c22da5eb3ca32e7705a2def2
nimai <nimai.m.patel@gmail.com>
2020-10-15 06:43:05 +0530

    postfix eval

---
+++ data-structures/stack/examples/postfix_eval/postfix_eval.c
@@ -0,0 +1,135 @@
+#include <assert.h>
+#include <math.h>
+#include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int stack[100];
+int top = -1;
+
+bool is_empty() {
+    if (top == -1) {
+        return true;
+    }
+    return false;
+}
+
+bool is_operator(char c) {
+    if (c == '^' || c == '+' || c == '*' || c == '-' || c == '/') {
+        return true;
+    }
+    return false;
+}
+
+bool is_operand(char c) {
+    if (c >= 'a' && c <= 'z') {
+        return true;
+    }
+    if (c >= 'A' && c <= 'Z') {
+        return true;
+    }
+    return false;
+}
+
+int pres(char c) {
+    assert(is_operator(c));
+    if (c == '^') {
+        return 2;
+    } else if (c == '/' || c == '*') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void push(int c) {
+    stack[++top] = c;
+}
+
+void pop() {
+    if (top == -1) {
+        printf("Popping from empty stack!");
+        return;
+    } else {
+        top--;
+    }
+}
+
+int peek() {
+    if (top == -1) {
+        printf("Peeking empty stack!");
+        return '\0';
+    }
+    return stack[top];
+}
+
+void append_char(char* s, char c) {
+    char* iter = s;
+    while (*iter != '\0') {
+        iter++;
+    }
+    *iter = c;
+}
+
+int perform(char oper, int a, int b) {
+    assert(is_operator(oper));
+    switch (oper) {
+        case '^':
+            return pow(a, b);
+            break;
+        case '*':
+            return a * b;
+            break;
+        case '/':
+            return a / b;
+            break;
+        case '+':
+            return a + b;
+            break;
+        case '-':
+            return a - b;
+            break;
+    }
+    return 0;
+}
+
+int eval_postfix(char* postfix) {
+    char* iter = postfix;
+    while (*iter != '\0') {
+        if (*iter == ' ') {
+            continue;
+        }
+        if (is_operand(*iter)) {
+            char substring[10] = "";
+            while (*iter != ' ') {
+                append_char(substring, *iter);
+                iter++;
+            }
+            push(atoi(substring));
+            printf("%s", substring);
+        } else if (is_operator(*iter)) {
+            int operand1 = peek();
+            pop();
+            int operand2 = peek();
+            pop();
+            int result = perform(*iter, operand1, operand2);
+            push(result);
+        }
+        iter++;
+    }
+    return peek();
+}
+
+int main() {
+    char postfix[100] = "12 24 +";
+    /*char* iter = postfix;*/
+    /*char substring[10] = "";*/
+    /*while (*iter != ' ') {*/
+    /*append_char(substring, *iter);*/
+    /*iter++;*/
+    /*}*/
+    /*printf("%d", atoi(substring));*/
+    int res = eval_postfix(postfix);
+    printf("%d", res);
+}
