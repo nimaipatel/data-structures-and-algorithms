3bf071980341cc86f5e02b169a405a59e70446be
nimai <nimai.m.patel@gmail.com>
2021-04-10 19:22:10 +0530

    LCS length

---
+++ algorithms/longest-common-subseq/lcs.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int
+max(int a, int b)
+{
+	return a > b ? a : b;
+}
+
+int
+lcs(char *str1, char *str2)
+{
+	int m = strlen(str1);
+	int n = strlen(str2);
+	int l[m + 1][n + 1];
+
+	for (int i = 0; i < m + 1; ++i) {
+		for (int j = 0; j < n + 1; ++j) {
+			if (i == 0 || j == 0)
+				l[i][j] = 0;
+			else if(str1[i - 1] == str2 [j - 1])
+				l[i][j] = l[i - 1][j - 1] + 1;
+			else
+				l[i][j] = max(l[i - 1][j], l[i][j - 1]);
+		}
+	}
+	 
+	return l[m][n];
+}
+
+int
+main()
+{
+	char *string1 = "HAVANA";
+	char *string2 = "SAVANNAH";
+	printf("%d", lcs(string1, string2));
+}
